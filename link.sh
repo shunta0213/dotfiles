30b716acbe11d057666878603bc9b75b7aef6d13
shunta0213 <shutnaide16213@gmail.com>
2023-09-06 18:29:56 +0900

    change script

--- link.sh
+++ link.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
+set -ue
 
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+link_to_homedir() {
+	command echo "backup old dotfiles..."
+	if [ ! -d "$HOME/dotfiles.bak" ];then
+		command echo "$HOME/.dotbackup not found. Auto Make it"
+    		command mkdir "$HOME/dotfiles.bak"
+  	fi
+
+	for f in ${HOME}/dotfiles/.??*; do
+		local fname=`basename ${f}`
+
+		# skip for git
+      		[[ ${fname} == ".git" ]] && continue
+
+		# make backup
+      		if [[ -e "${HOME}/${fname}" ]];then
+			command echo making buckup ${HOME}/${fname}
+        		command mv "${HOME}/${fname}" "${HOME}/dotfiles.bak"
+      		fi
+
+      		command ln -snf $f $HOME
+    	done
+}
+
+
+link_to_homedir
+command echo -e "\e[1;36m Install completed!!!! \e[m"
