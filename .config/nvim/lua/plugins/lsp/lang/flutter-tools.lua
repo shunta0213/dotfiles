8941456520b935ec6e750d459554179c5dc8903b
shunta0213 <shutnaide16213@gmail.com>
2024-02-08 19:32:52 +0900

    add nvim plugins

--- .config/nvim/lua/plugins/lsp/lang/flutter-tools.lua
+++ .config/nvim/lua/plugins/lsp/lang/flutter-tools.lua
@@ -5,7 +5,5 @@ return {
 		"nvim-lua/plenary.nvim",
 		"stevearc/dressing.nvim", -- optional for vim.ui.select
 	},
-	config = function()
-		require("flutter-tools").setup({})
-	end,
+	opts = {},
 }
--- .config/nvim/lua/plugins/lsp/luasnip.lua
+++ .config/nvim/lua/plugins/lsp/luasnip.lua
@@ -1,5 +1,5 @@
 return {
 	"L3MON4D3/LuaSnip",
-	version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	version = "v2.*",
 	build = "make install_jsregexp",
 }
